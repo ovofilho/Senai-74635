fdcfc644df40c8f080a218630d5a815d5abc58f1
Aluno <Aluno@C302-LAB14-001>
2022-05-09 14:49:31 -0300

    Primeiro Commit trabalho final BD e DSII

--- Banco/src/banco/Poupanca.java
+++ Banco/src/banco/Poupanca.java
@@ -5,10 +5,56 @@
  */
 package banco;
 
+
 /**
  *
  * @author Aluno
  */
-public class Poupanca {
+public class Poupanca extends Conta{
+    private int dataRendimento;
+    
+    public Poupanca(int idG, int idC,int nr, Double saldo){
+        
+        super(idG,idC,nr, saldo);
+        dataRendimento = 1;
+    }
+
+   
+    public void alteraStatus(int nrG, boolean status) {
+        if(super.getIdGerente() == nrG){
+            super.setStatus(status);
+            
+        }
+    }
+
+ 
+    public void sacar(int id, Double valor) {
+        if(super.getIdCliente()==id){
+            //pode sacar
+        }
+        else{
+            System.out.println("Saque não autorizado");
+        }
+    }
+
+    @Override
+    public void depositar(Double valor) {
+        
+    }
+
+    @Override
+    public void transferir(int nrBanco, int nrAgencia, int nrConta, Double valor) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void alteraStatus() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void sacar(Double valor) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
